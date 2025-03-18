e146198c94491357f7bf0d256bb30eff0f13c27f
Mohammad Ramzan <mram457456@gmail.com>
2025-03-18 20:55:25 +0500

    basic task manager

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import TaskList from "./Components/TaskList";
+
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+    <TaskList />
+    </div>
+  );
+}
